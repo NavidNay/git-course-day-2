b36ec9ff680f981d97c32c00a1663c0909e28861
NavidNay <nayvisquad@gmail.com>
2021-02-05 10:47:45 +0100

    Added decrement method

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,17 @@ public class Main {
 
         int increment = increment(num);
 
-        int newNumber = increment(increment);
+        int decrement = decrement(increment);
 
         System.out.println(increment);
-        System.out.println(newNumber);
+        System.out.println(decrement);
     }
 
     public static int increment(int number) {
         return ++number;
+    }
+
+    public static int decrement(int number) {
+        return --number;
     }
 }
